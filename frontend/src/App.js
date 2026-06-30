e05fc4e7a47da40da218e4f8cf4f80db37fab29d
Mubarak Lawal <mubaraklawal52@gmail.com>
2026-06-30 11:19:32 +0100

    setup yarn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react"
 import "./App.css"
 import { connect, sendMsg } from "./api"
+import Header from "./components/Header";
 
 class App extends Component{
   constructor(props){
@@ -17,6 +18,7 @@ class App extends Component{
   render() {
     return (
       <div className="App">
+        <Header />
         <button onClick={this.send}>Hit</button>
       </div>
     );
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Header.css";
+const Header = () => (
+    <div className="header">
+        <h1>Chat App</h1>
+    </div>
+);
+
+export default Header;
